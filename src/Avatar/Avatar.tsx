de762926d405768328028986542b86667614efe9
kubilaysalih <kubilay@kubilay.io>
2024-11-27 12:55:09 +0300

    Fix avatar

--- src/Avatar/Avatar.tsx
+++ src/Avatar/Avatar.tsx
@@ -1,12 +1,12 @@
 export interface AvatarProps {
-  title: string
+  size: number
 }
 
-const Avatar = ({ title }: AvatarProps) => {
+const Avatar = ({ size = 50 }: AvatarProps) => {
   return <img src="https://lh3.googleusercontent.com/a/ACg8ocIOSNCy2t2tpe_14dxvnmEMAA_4cZjBGK6q3PuqbFDJO9Q=s96-c" style={{
-    width: '50px',
-    height: "50px"
-  }}>{title}</img>
+    width: size,
+    height: size
+  }} />
 }
 
 export default Avatar
